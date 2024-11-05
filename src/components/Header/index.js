22b815ef26664dd6d3fbe30d451f8b61d0e2b95b
piyaya06 <monoapril7@gmail.com>
2024-11-05 09:40:14 +0800

    adding generation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,13 @@
-import Generations from './Generations';
-import Logo from './Logo';
-export { Generations, Logo};
+import Logo from "./Logo";
+import Generations from "./Generations";
+const Header = ({ text }) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+      <Logo text="Logo props" />
+      <Generations text="Generations props" />
+    </div>
+  );
+};
+
+export default Header;
