e5e64118e9a9c5040e4336a7cbd05bf9adfc1914
piyaya06 <monoapril7@gmail.com>
2024-11-14 10:09:47 +0800

    Adding feature in components

---
+++ src/components/Feature/index.js
@@ -0,0 +1,42 @@
+const Feature = () => {
+  return (
+    <section id="feature">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-4">
+            <div className="feature-thumb">
+              <span>01</span>
+              <h3>Trending Courses</h3>
+              <p>
+                Known is a free education HTML Bootstrap template. You can
+                modify it in any way and use it for your website.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-4">
+            <div className="feature-thumb">
+              <span>02</span>
+              <h3>Books & Library</h3>
+              <p>
+                You are allowed to use Known HTML Template for your commercial
+                or non-commercial websites.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-4">
+            <div className="feature-thumb">
+              <span>03</span>
+              <h3>Certified Teachers</h3>
+              <p>
+                Please spread the word about us. Template redistribution is NOT
+                allowed on any download website.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Feature;
