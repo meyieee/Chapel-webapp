b54aba57a9a45324f5ce6aa84415a8c04acdb89d
arturrr03 <rawungito@gmail.com>
2024-11-17 16:24:48 +0800

    feat: scroll smooth in header

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { ref, onValue } from '@firebase/database';
-import { database } from '../../config/FIrebase';
-import { useState, useEffect } from 'react';
+import { ref, onValue } from "@firebase/database";
+import { database } from "../../config/Firebase/index";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const [homeContent, setHomeContent] = useState({
     title: "YOUR OPINION MATTERS",
     subtitle: "Give us your best thoughts about the student forum",
-    description: "Join our vibrant community where every voice counts. Share your experiences, ideas, and suggestions to help shape the future of our student forum."
+    description:
+      "Join our vibrant community where every voice counts. Share your experiences, ideas, and suggestions to help shape the future of our student forum.",
   });
 
   useEffect(() => {
-    const homeRef = ref(database, 'home');
+    const homeRef = ref(database, "home");
     const unsubscribe = onValue(homeRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./Assets/css/style.css";
-import "./config/FIrebase";
+import "./config/Firebase/index";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
