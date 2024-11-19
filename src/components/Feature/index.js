ec8463d9c10415f02d48720ea584f10f639129e8
piyaya06 <monoapril7@gmail.com>
2024-11-19 10:26:22 +0800

    adding photos in feature

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,40 +1,52 @@
 import { getDatabase, ref, onValue } from "firebase/database";
 import { useEffect, useState } from "react";
+
 const Feature = () => {
   const [feature, setFeature] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
 
   useEffect(() => {
+    // Ambil data feature dan gambar dari Firebase Realtime Database
     const db = getDatabase();
     const featureRef = ref(db, "feature/");
 
     onValue(featureRef, (snapshot) => {
       const data = snapshot.val();
       setFeature(data);
+
+      // Mengambil URL gambar dari Firebase Realtime Database
+      const images = [data.image1, data.image2, data.image3];
+      setImageUrls(images);
     });
   }, []);
+
   return (
     <section id="feature">
       <div className="container">
         <div className="row">
-          <div className="col-md-4 col-sm-4">
+          {/* Feature 1 with 3 images in horizontal layout */}
+          <div className="col-md-12">
             <div className="feature-thumb">
               <span>01</span>
               <h3>{feature.title}</h3>
               <p>{feature.subTitle}</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-4">
-            <div className="feature-thumb">
-              <span>02</span>
-              <h3>{feature.title1}</h3>
-              <p>{feature.subTitle1} </p>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-4">
-            <div className="feature-thumb">
-              <span>03</span>
-              <h3>{feature.title2}</h3>
-              <p>{feature.subTitle2}</p>
+
+              {/* Images displayed horizontally */}
+              <div className="feature-images">
+                {imageUrls.length > 0 ? (
+                  imageUrls.map((url, index) => (
+                    <div className="feature-image" key={index}>
+                      <img
+                        src={`data:image/jpeg;base64,${url}`} // Menampilkan gambar dalam format base64
+                        alt={`Feature ${index + 1}`}
+                        className="img-fluid"
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <p>Loading images...</p>
+                )}
+              </div>
             </div>
           </div>
         </div>
